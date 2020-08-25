c62eeb60153c45042d185a669b2f23acf156d196
comalmot <comalmot@kakao.com>
2020-08-25 11:04:12 -0700

    No Ga Da

---
+++ netfilter-test.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>		/* for NF_ACCEPT */
+#include <errno.h>
+#include <string.h>
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
+
+char URL[4096] = {'\0'};
+
+int check_Method(unsigned char* buf) {
+    if(buf[0] == 'G' && buf[1] == 'E' && buf[2] == 'T')
+        return 1;
+    //if(buf[0] == 'H' && buf[1] == 'O' && buf[2] == 'S' && buf[3] == 'T')
+    //    return 2;
+    else
+        return 0;
+    
+}
+
+int dump(unsigned char* buf, int size) {
+	int i;
+    unsigned char pkt_host[4096];
+    int option = 0;
+    //printf("Dump\n");
+    printf("==========================\n");
+    printf("%s\n\n", buf);
+    printf("==========================\n");
+
+    option = check_Method(buf);
+
+    switch (option) {
+        case 1: sscanf(buf, "GET / HTTP/1.1\r\nHost: %s", pkt_host);
+            break;
+        default:
+            break;
+    }
+
+    if(option) {
+        printf("Check Method Pass\n");
+        printf("%s\n", pkt_host);
+
+        if(!strncmp(pkt_host, URL, strlen(pkt_host))) {
+            return 1;
+        }
+        else
+            return 0;
+    }
+}
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+	      struct nfq_data *nfa, void *data)
+{
+	int id = 0;
+	struct nfqnl_msg_packet_hdr *ph;
+	struct nfqnl_msg_packet_hw *hwph;
+	u_int32_t mark,ifi;
+	int ret;
+	unsigned char *pkt;
+	char *Method;
+    char *URI;
+    char *URL;
+    int op = 0;
+	ph = nfq_get_msg_packet_hdr(nfa);
+	
+	if (ph) {
+		id = ntohl(ph->packet_id);
+	}
+	
+	ret = nfq_get_payload(nfa, &pkt);
+	if (ret >= 0) {
+		//printf("payload_len=%d ", ret);
+        pkt += 0 + 20 + 20; // ip tcp ~
+		op = dump(pkt, ret);	
+	}
+	//fputc('\n', stdout);
+	//printf("entering callback\n");
+    if(op) {
+        printf("DROPPED!\n");
+	    return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
+    }
+
+    else {
+        return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+    }
+}
+
+int main(int argc, char **argv)
+{
+	struct nfq_handle *h;
+	struct nfq_q_handle *qh;
+	struct nfnl_handle *nh;
+	int fd;
+	int rv;
+	char buf[4096] __attribute__ ((aligned));
+
+    if(argc != 2) {
+        printf("Usage : ./real <url>\n");
+    }
+
+    strncpy(URL, argv[1], 4095);
+	printf("opening library handle\n");
+	h = nfq_open();
+	if (!h) {
+		fprintf(stderr, "error during nfq_open()\n");
+		exit(1);
+	}
+
+	printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
+	if (nfq_unbind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_unbind_pf()\n");
+		exit(1);
+	}
+
+	printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+	if (nfq_bind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_bind_pf()\n");
+		exit(1);
+	}
+
+	printf("binding this socket to queue '0'\n");
+	qh = nfq_create_queue(h,  0, &cb, NULL);
+	if (!qh) {
+		fprintf(stderr, "error during nfq_create_queue()\n");
+		exit(1);
+	}
+
+	printf("setting copy_packet mode\n");
+	if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+		fprintf(stderr, "can't set packet_copy mode\n");
+		exit(1);
+	}
+
+	fd = nfq_fd(h);
+
+	for (;;) {
+		if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
+			//printf("pkt received\n");
+			nfq_handle_packet(h, buf, rv);
+			continue;
+		}
+		/* if your application is too slow to digest the packets that
+		 * are sent from kernel-space, the socket buffer that we use
+		 * to enqueue packets may fill up returning ENOBUFS. Depending
+		 * on your application, this error may be ignored. nfq_nlmsg_verdict_putPlease, see
+		 * the doxygen documentation of this library on how to improve
+		 * this situation.
+		 */
+		if (rv < 0 && errno == ENOBUFS) {
+			printf("losing packets!\n");
+			continue;
+		}
+		perror("recv failed");
+		break;
+	}
+
+	printf("unbinding from queue 0\n");
+	nfq_destroy_queue(qh);
+
+#ifdef INSANE
+	/* normally, applications SHOULD NOT issue this command, since
+	 * it detaches other programs/sockets from AF_INET, too ! */
+	printf("unbinding from AF_INET\n");
+	nfq_unbind_pf(h, AF_INET);
+#endif
+
+	printf("closing library handle\n");
+	nfq_close(h);
+
+	exit(0);
+}
+
